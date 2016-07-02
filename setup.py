edb2705568f5d92c879acdd6f81b3e10a9f9b9fb
ictxiangxin <ictxiangxin@gmail.com>
2016-07-03 00:03:50 +0800

    do not use readme for long_description

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ install_requires = [
 ]
 
 if __name__ == "__main__":
-    with open("README.md") as fp:
-        long_description = fp.read()
+    long_description = "%s - %s" % (configure.boson_title, configure.boson_description)
 
     setup(name=configure.boson_package_name,
           version="%d.%d" % (configure.boson_version_main, configure.boson_version_sub),
@@ -30,4 +29,5 @@ if __name__ == "__main__":
                        "Operating System :: Microsoft :: Windows",
                        "Operating System :: Unix",
                        "Operating System :: MacOS",
-                       "Programming Language :: Python :: 3"], )
+                       "Programming Language :: Python :: 3"],
+          zip_safe=False)
